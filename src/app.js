aeae32d8bfa3ad85750f0db5ac2fca9b6c5e4f62
valefresia <131785137+valefresia@users.noreply.github.com>
2026-08-06 17:28:44 -0300

    agregue el swagger con copilot

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const path = require("path");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
 const authorsRouter = require("./routes/authors.routes");
 const postsRouter = require("./routes/posts.routes");
 const errorHandler = require("./middlewares/errorHandler");
@@ -7,8 +10,15 @@ const app = express();
 
 app.use(express.json());
 
+const swaggerDocument = YAML.load(path.join(__dirname, "../docs/swagger.yaml"));
+
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok", timestamp: new Date().toISOString() });
+});
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get("/swagger.yaml", (req, res) => {
+  res.sendFile(path.join(__dirname, "../docs/swagger.yaml"));
 });
 
 app.use("/authors", authorsRouter);
